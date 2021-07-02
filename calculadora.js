3e3c78d76881356d80fe0edb6dd9627cdc9149cd
Ivan Ruiz <ivanrur95@hotmail.com>
2021-07-02 13:30:00 -0500

    en desarrollo de calculadora

---
+++ calculadora.js
@@ -0,0 +1,13 @@
+let muroAncho = document.getElementById('muro_ancho')
+let muroAlto = document.getElementById('muro_alto')
+let resultado1 = null
+let totalMuro = document.getElementById('calcular')
+
+let btnCalcular = document.getElementById('resultadoMuro')
+
+btnCalcular.addEventListener('click', function(){
+
+    suma = parseFloat(muroAncho.value)+parseFloat(muroAlto.value)
+    console.log(suma);
+    
+})  
